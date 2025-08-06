d60084024abe9872f77f3696134284fb9d8f4d8f
jinseungbok <92.bokjs@gmail.com>
2025-08-06 13:01:48 +0900

    "[docs]:250806_Enum EnumUserRole"

---
+++ src/main/java/kr/co/green/greengram/config/enumcode/EnumUserRole.java
@@ -0,0 +1,18 @@
+package kr.co.green.greengram.config.enumcode;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum EnumUserRole implements EnumMapperType{
+    USER_1("01", "유저1")
+  , USER_2("02", "유저2")
+  , ADMIN("03", "관리자")
+  , MENTO("04", "멘토")
+  , MANAGER("05", "매니저")
+  ;
+
+    private final String code;
+    private final String value;
+}
