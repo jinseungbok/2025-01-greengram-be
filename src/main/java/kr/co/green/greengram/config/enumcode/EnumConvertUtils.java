627f4403e1dacba14cc1cb167269008bdfb51721
jinseungbok <92.bokjs@gmail.com>
2025-08-06 12:46:41 +0900

    "[docs]:250806_EnumMapper, EnumConvertUtils"

---
+++ src/main/java/kr/co/green/greengram/config/enumcode/EnumConvertUtils.java
@@ -0,0 +1,29 @@
+package kr.co.green.greengram.config.enumcode;
+
+import io.micrometer.common.util.StringUtils;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.util.EnumSet;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE) // 객체화를 막기 위함
+public class EnumConvertUtils {
+    // String(Code 값) to Enum
+    // 리턴 타입이 Enum이어야 하고 EnumMapperType를 상속받은 enum이어야 한다.
+    // enumClass는 Enum 타입이어야 한다.
+    public static <E extends Enum<E> & EnumMapperType> E ofCode(Class<E> enumClass, String code) {
+        if(StringUtils.isBlank(code)) { return null; } // 매개변수가 null이거나 빈칸인 경우 return null 처리
+
+        // Enum에 있는 값 중 매개변수 code와 같은 값을 찾아 리턴하기 위함. 근데 같은 게 없으면 return null
+        return EnumSet.allOf(enumClass).stream() //enum을 Stream화 하기 위함
+                .filter(item -> item.getCode().equals(code))
+                .findFirst()
+                .orElse(null);
+    }
+
+    // Enum to String(Code 값)
+    public static <E extends Enum<E> & EnumMapperType> String toCode(E enumItem) {
+        if (enumItem == null) { return null; }
+        return enumItem.getCode();
+    }
+}
--- src/main/java/kr/co/green/greengram/config/enumcode/EnumMapper.java
+++ src/main/java/kr/co/green/greengram/config/enumcode/EnumMapper.java
@@ -4,6 +4,7 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
+// 내부적으로 관리하는 공통코드를 요청을 통해 확인하고 싶을 때 사용
 public class EnumMapper {
     private Map<String, List<EnumMapperValue>> factory = new LinkedHashMap<>();
 
@@ -12,7 +13,8 @@ public class EnumMapper {
         factory.put(key, toEnumValues(e));
     }
 
-    private List<EnumMapperValue> toEnumValues(Class<? extends EnumMapperValue> e) {
+    private List<EnumMapperValue> toEnumValues(Class<? extends EnumMapperType> e) {
+        e.getEnumConstants(); // 특정 enum 타입이 갖고 있는 모든 값을 출력시키는 기능은 class의 getEnumConstants() 메소드 사용
         return null;
     }
 }
