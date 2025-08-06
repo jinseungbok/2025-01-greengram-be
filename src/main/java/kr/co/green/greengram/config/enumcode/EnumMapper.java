a7a446c970a319a7582166931ff5b836d2b492b5
jinseungbok <92.bokjs@gmail.com>
2025-08-06 12:16:54 +0900

    "[docs]:250806_enum"

---
+++ src/main/java/kr/co/green/greengram/config/enumcode/EnumMapper.java
@@ -0,0 +1,9 @@
+package kr.co.green.greengram.config.enumcode;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public class EnumMapper {
+    private Map<String, List<EnumMapperValue>> factory = new LinkedHashMap<>();
+}
